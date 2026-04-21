c23e66cc9750ab43aa33801acd21582f89c5ee52
tayeromney-pixel <taye.romney@gmail.com>
2026-04-21 15:23:24 +0000

    9455

---
+++ naming-variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+ int number; //1 change the ! after the number
+int test_value; // 1 put the_between test and value
+int example1; //moved number to end
+float int_float; //keyword not full name
+printf("Variables created successfully.\n");
+}
--- recipe.c
+++ recipe.c
@@ -3,5 +3,11 @@
 int main(void) {
     printf("Coconut Cranberry Dessert\n" );
     printf("\n");
-}
+    printf("1 1/2 cups graham cracker crumbs\n");
+    printf("1/2 cup butter or margarine, melted\n);
+    printf("1 can sweetened condensed milk\n")
+    }
+
+
+
 
