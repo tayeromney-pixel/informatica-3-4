8559e4341e91cae09ede52dc1e972df10a30935e
tayeromney-pixel <taye.romney@gmail.com>
2026-04-22 15:13:47 +0000

    april20

--- recipe.c
+++ recipe.c
@@ -3,5 +3,18 @@
 int main(void) {
     printf("Coconut Cranberry Dessert\n" );
     printf("\n");
+    printf("1 1/2 cups graham cracker crumbs\n");
+    printf("1/2 cup butter or margarine, melted\n");
+    printf("1 can sweetened condensed milk\n");
+    printf("1 cup flaked coconut\n");
+    printf("1 cup dried cranberries\n");
+    printf("2 cups vanilla chips\n");
+    printf("1 cup sliced almonds or pecans\n");
+    printf("\n");
+    printf("Combine graham cracker crumbs and butter. Press into a greased 13″ x 9″ (33 x 23-cm) pan.\n");
+printf("Combine remaining ingredients and spread over graham cracker mixture. Bake at 350°F (175°C) for 25 minutes or until the edges are golden brown. Let cool and cut into squares.\n");
 }
+
+
+
 
