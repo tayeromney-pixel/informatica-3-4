0bcffc1e38b91a9f676c3101ccd6bff0b9782331
tayeromney-pixel <taye.romney@gmail.com>
2026-04-29 14:56:29 +0000

    test

---
+++ test/reciept.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(void)
+{
+    float item
+    float price
+    int quantity
+    char total
+
+printf("Name of purchased item:\n");
+scanf("%s",&item);
+
+printf("Price:\n");
+scanf("%s",&item);
+
+printf("Quantity:\n");
+scanf("%s",&item);
+
+total = (price * quantity);
+
+printf("**************************************\n");
+printf("Techdraui Grocery Store");
+printf("**************************************\n");
+printf("Item\n");
+printf("Quantity : %d\n", quantity);
+printf("Price: %.2f\n", price);
+printf("--------------------------------------");
+printf("total");
+printf("--------------------------------------");
+printf("Thank you for your purchase");
+
+
+}
