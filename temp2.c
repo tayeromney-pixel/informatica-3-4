9f9c5c33dbf9cc592434f2b3f4de640aec5cd43f
tayeromney-pixel <taye.romney@gmail.com>
2026-05-07 20:19:04 +0000

    not finished

---
+++ temp2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float celsius;
+    float fahrenheit;
+    printf("Enter temp in celsius:");
+    scanf("%c", &Celsius);
+    fahrenheit = (celsius * 1.8) + 32
+    if(celsius<0){
+        printf("freezing temperature");
+        elseif(celsius>10){
+            
+
+
+    printf("fahrenhiet: %.0f\n", fahrenhiet);
+
+    }
+
